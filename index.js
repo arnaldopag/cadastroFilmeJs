94e1106cc302b82816ebac1bb6c55d39c3110a03
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-05-26 22:03:10 -0300

    primeira versão

---
+++ index.js
@@ -0,0 +1,17 @@
+var cadastro = {
+    "nome": null,
+    "estilo": null,
+    "url": null,
+}
+
+const botao  = document.querySelector('button')
+botao.addEventListener('click', cadastrar())
+
+
+function cadastrar(){
+
+    for (let i = 0; i<=1;i++){
+    cadastro filme[i] = new cadastro();
+    
+    }
+}
