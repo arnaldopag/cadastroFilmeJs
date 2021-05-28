34f817d1b068025667d0d1569ec8e2833b0de60a
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-05-27 21:41:11 -0300

    ta melhorando

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-var cadastro = {
-    "nome": null,
-    "estilo": null,
-    "url": null,
-}
+function filme(nome,estilo,url){
+    this.nome = nome;
+    this.estilo = estilo;
+    this.url = url;
+    }
 
+var filmes = [{
+    'nome': null,
+    'estilo': null,
+    'url': null,
+    }   
+]
+
+let cont = 0
 const botao  = document.querySelector('button')
-botao.addEventListener('click', cadastrar())
-
+botao.addEventListener('click', cadastrar)
 
 function cadastrar(){
+    let name = document.querySelector('#name')
+    let gen = document.querySelector('#gen')
+    let img = document.querySelector('#img')
 
-    for (let i = 0; i<=1;i++){
-    cadastro filme[i] = new cadastro();
-    
+    filmes.push(new filme(name.value, gen.value, img.value))
+    cont++
+    mostrar(cont)
+}
+
+function mostrar(cont){
+    cont -= 1 
+     for(let i = 0; i<=cont; i++){
+        console.log(filmes[i])
     }
 }
